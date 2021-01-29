bbd3c41c284307687f413deff1b10d890a1bcddc
Timothy Langham <graniteblood@gmail.com>
2021-01-28 20:36:59 -0800

    finished task 1

--- index.js
+++ index.js
@@ -41,9 +41,26 @@ class Airplane {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- class Person {
-    
+
+class Person {
+  constructor (name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+    }
+  eat(someFood) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    }
   }
+  toString() {
+    this.stomach = [];
+    }
+  }
+
+
+ 
+
   
   /*
     TASK 2
@@ -59,9 +76,24 @@ class Airplane {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- class Car {
-    
+  class Car {
+    constructor = (model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+    }
   }
+
+  class fill(gallons) = function(gallons) {
+    this.tank += gallons;
+  }
+
+  class car(distance) = function(distance) {
+
+  }
+
+
   
   /*
     TASK 3
@@ -75,6 +107,7 @@ class Airplane {
           + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
           + {name} and {location} of course come from the instance's own properties.
   */
+
  class Lambdasian {
     
   }
@@ -93,9 +126,18 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
+
+
+
  class Instructor {
 
  }
+
+
+
+
+
+
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
@@ -111,10 +153,15 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
+
+
  class Student {
      
  }
   
+
+
+
   /*
     TASK 6
       - Write a ProjectManager class extending Instructor.
@@ -128,9 +175,15 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
+
+
+
  class ProjectManager {
      
  }
+
+
+
   /*
     STRETCH PROBLEM (no tests!)
       - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
@@ -143,6 +196,8 @@ class Airplane {
 
   //End of Challenge
   /* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
+
+
   function foo(){
     return 'bar';
 }
